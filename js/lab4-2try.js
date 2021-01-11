c7b640e65f3a20ae42dec818e1e926b05ab7c44d
MingaYe <71955061+MingaYe@users.noreply.github.com>
2021-01-11 18:24:26 +0800

    Update lab4-2try.js

--- js/lab4-2try.js
+++ js/lab4-2try.js
@@ -9,33 +9,26 @@ var s = [1.0, 1.0, 1.0];//缩放
 var theta = [0.0, 0.0, 0.0];
 var d = [0.0, 0.0, 0.0];
 
+var points = [];
+var colors = [];
+
 var maxNumVertices = 500;
 var index = 0;
 var side = 6;
 
-var cIndex = 0;
+var cIndex = 0;//颜色选择
 //var vIndex = 0;
-var dIndex = -1;
+var dIndex = -1;//图案选择
 
 var thetaLoc;//各种Loc
 var sLoc;
 var tLoc;
 var dLoc;
 
-var trianglePoints = [//三角形的图形
-    0.0, 0.1, 0.0,1.0,
-    -0.1, -0.1, 0.0,1.0,
-    0.1, -0.1, 0.0,1.0
-];
-var squarePoints = [//矩形的图形
-    0.0, 0.1, 0.0, 1.0,
-    -0.1, 0.0, 0.0, 1.0,
-    0.1, 0.0, 0.0, 1.0,
-    -0.1, 0.0, 0.0, 1.0,
-    0.1, 0.0, 0.0, 1.0,
-    0.0, -0.1, 0.0, 1.0
-];
+var trianglePoints = [];//三角形的图形
+var squarePoints = [];//矩形的图形
 var circlePoints = [];//圆
+var cubePoints = [];//立方体
 
 var colors = [
 	0.0, 0.0, 0.0, 1.0, // black
@@ -55,6 +48,8 @@ var start = [0];
 var circleColors = [];
 var triangleColors = [];
 var squareColors = [];
+var cubeColors = [];//立方体
+
 //----------------------------------------------------------------------------
 
 function init(){
@@ -93,7 +88,7 @@ function init(){
 	tLoc = gl.getUniformLocation(program,"pos");//平移
 	thetaLoc = gl.getUniformLocation(program, "theta");//旋转
 	sLoc = gl.getUniformLocation(program, "s");//缩放
-	dLoc = gl.getUniformLocation(program, "pos");
+	dLoc = gl.getUniformLocation(program, "d");
 	
 	
 	//--------------------------------选图案--------------------------------------------
@@ -103,7 +98,6 @@ function init(){
 		console.log(dIndex);
 	});
 	
-	
 	//-----------------------------选颜色-----------------------------------------
 	var m = document.getElementById( "cmenu" );
 	m.addEventListener( "click", function(){
@@ -112,15 +106,15 @@ function init(){
 	});
 	
 	//------------------------------------------边---------------------------------------
-	side = document.getElementById("bs").value;
-
+	
 	//-------------------------------鼠标点击--------------------------------------------
 	canvas.addEventListener( "mousedown", function( event ){
 		triangleColors = [];
 		circleColors = [];
 		squareColors = [];
+		cubeColors = [];
 		huatu();
-		console.log(circlePoints);
+		console.log(cubePoints);
 		var rect = canvas.getBoundingClientRect();
 		var cx = event.clientX - rect.left;
 		var cy = event.clientY - rect.top; 
@@ -131,8 +125,12 @@ function init(){
 }
 //-------------------------------------------------------------------------------------------
 function huatu(){
-	if(dIndex == 0){
-		console.log("a");
+	if(dIndex == 0){//三角形
+		trianglePoints = [
+		    -0.1, 0.0, 0.0,1.0,
+		    0.1, 0.0, 0.0,1.0,
+		    0.0, Math.sqrt(3)/10, 0.0,1.0
+		];
 		gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
 		gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(trianglePoints), gl.STATIC_DRAW);
 		gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
@@ -153,7 +151,14 @@ function huatu(){
 		//t = glMatrix.vec4.fromValues( colors[c], colors[c+1], colors[c+2], colors[c+3] );
 		//gl.bufferSubData( gl.ARRAY_BUFFER, 16 * 3, new Float32Array( t ) );
 	}else if(dIndex == 1){
-		console.log("b");
+		squarePoints = [//矩形的图形
+		    0.0, 0.1, 0.0, 1.0,
+		    -0.1, 0.0, 0.0, 1.0,
+		    0.1, 0.0, 0.0, 1.0,
+		    -0.1, 0.0, 0.0, 1.0,
+		    0.1, 0.0, 0.0, 1.0,
+		    0.0, -0.1, 0.0, 1.0
+		];
 		gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
 		gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(squarePoints), gl.STATIC_DRAW);
 		//gl.bufferSubData( gl.ARRAY_BUFFER,16 * 6 ,new Float32Array(squarePoints));
@@ -174,9 +179,9 @@ function huatu(){
 		//c = c * 4;
 		//t = glMatrix.vec4.fromValues( colors[c], colors[c+1], colors[c+2], colors[c+3] );
 		//gl.bufferSubData( gl.ARRAY_BUFFER, 16 * 6, new Float32Array( t ) );
-	}else if(dIndex == 2){
-		console.log("c");
-		side = 100;
+	}else if(dIndex == 2){//圆
+		side = document.getElementById("bs").value;
+		circlePoints=[];
 		for(var i = 0; i < side; i++){
 			var thetas = i * 2 * Math.PI / side;
 			var r = 0.1;
@@ -196,6 +201,52 @@ function huatu(){
 		//c = c * 4;
 		//t = glMatrix.vec4.fromValues( colors[c], colors[c+1], colors[c+2], colors[c+3] );
 		//gl.bufferSubData( gl.ARRAY_BUFFER, 16 * 6, new Float32Array( t ) );
+	}else if(dIndex == 3){//立方体
+		points = [
+		    glMatrix.vec4.fromValues(-0.1, -0.1, 0.1, 0.2),
+		    glMatrix.vec4.fromValues(-0.1, 0.1, 0.1, 0.2),
+		    glMatrix.vec4.fromValues(0.1, 0.1, 0.1, 0.2),
+		    glMatrix.vec4.fromValues(0.1, -0.1, 0.1, 0.2),
+		    glMatrix.vec4.fromValues(-0.1, -0.1, -0.1, 0.2),
+		    glMatrix.vec4.fromValues(-0.1, 0.1, -0.1, 0.2),
+		    glMatrix.vec4.fromValues(0.1, 0.1, -0.1, 0.2),
+		    glMatrix.vec4.fromValues(0.1, -0.1, -0.1, 0.2),
+		];
+		
+		var Colors = [
+		    glMatrix.vec4.fromValues(0.0, 0.0, 0.0, 1.0),
+		    glMatrix.vec4.fromValues(1.0, 0.0, 0.0, 1.0),
+		    glMatrix.vec4.fromValues(1.0, 1.0, 0.0, 1.0),
+		    glMatrix.vec4.fromValues(0.0, 1.0, 0.0, 1.0),
+		    glMatrix.vec4.fromValues(0.0, 0.0, 1.0, 1.0),
+		    glMatrix.vec4.fromValues(1.0, 0.0, 1.0, 1.0),
+		    glMatrix.vec4.fromValues(0.0, 1.0, 1.0, 1.0),
+		    glMatrix.vec4.fromValues(1.0, 1.0, 1.0, 1.0)
+		];
+		
+		var faces = [
+		    1, 0, 3, 1, 3, 2, //正
+		    2, 3, 7, 2, 7, 6, //右
+		    3, 0, 4, 3, 4, 7, //底
+		    6, 5, 1, 6, 1, 2, //顶
+		    4, 5, 6, 4, 6, 7, //背
+		    5, 4, 0, 5, 0, 1  //左
+		];
+		
+		for (var i = 0; i < faces.length; i++) {
+		    cubePoints.push(points[faces[i]][0], points[faces[i]][1], points[faces[i]][2],points[faces[i]][3]);
+		
+		    cubeColors.push(Colors[Math.floor(i / 6)][0],
+						Colors[Math.floor(i / 6)][1],
+						Colors[Math.floor(i / 6)][2], 
+						Colors[Math.floor(i / 6)][3]);
+		}
+		console.log(cubePoints);
+		gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+		gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(cubePoints), gl.STATIC_DRAW);
+		gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+		gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(cubeColors), gl.STATIC_DRAW);
+		
 	}
 
 }
@@ -206,6 +257,7 @@ function renderPolygons(){
 	gl.clear( gl.COLOR_BUFFER_BIT );
 	
 	//for( var i = 0; i < numPolygons; i++ ){
+		gl.uniform3fv( tLoc,new Float32Array(t));
 		if(dIndex == 0){
 			s[0] += 0.02;
 			s[1] += 0.02;
@@ -215,25 +267,43 @@ function renderPolygons(){
 				s[1] = 0.5;
 				s[2] = 0.5;
 			}
-			
-			console.log("aa");
-			gl.uniform3fv( tLoc,new Float32Array(t));
+			theta = [0.0, 0.0, 0.0];
+			gl.uniform3fv( thetaLoc,new Float32Array(theta));
+			d=[0.0,0.0,0.0];
+			gl.uniform3fv( dLoc,new Float32Array(d));	
 			gl.uniform3fv( sLoc,new Float32Array(s));
 			gl.drawArrays( gl.TRIANGLES, 0, 3);
 		}else if(dIndex == 1){
+			gl.uniform3fv( tLoc,new Float32Array(t));
+			s=[1.0,1.0,1.0];
+			gl.uniform3fv( sLoc,new Float32Array(s));
 			theta[2] +=0.1;
 			gl.uniform3fv( thetaLoc,new Float32Array(theta));
-			
-			console.log("bb");
-			gl.uniform3fv( tLoc,new Float32Array(t));
+			d=[0.0,0.0,0.0];
+			gl.uniform3fv( dLoc,new Float32Array(d));
 			gl.drawArrays( gl.TRIANGLES, 0, 6);
 		}else if(dIndex == 2){
 			
-			//gl.uniform3fv(dLoc, d);
-			
-			console.log("cc");
+			s=[1.0,1.0,1.0];
+			gl.uniform3fv( sLoc,new Float32Array(s));
+			theta = [0.0, 0.0, 0.0];
+			gl.uniform3fv( thetaLoc,new Float32Array(theta));
+			d[0] += 0.02;
+			if(d[0]>1){
+				d[0] = 0.0;
+			}
+			gl.uniform3fv( dLoc,new Float32Array(d));
 			gl.uniform3fv( tLoc,new Float32Array(t));
-			gl.drawArrays( gl.TRIANGLE_FAN, 0, circlePoints.length/2);
+			gl.drawArrays( gl.LINE_LOOP, 0, circlePoints.length/2);
+		}else if(dIndex == 3){
+			theta[0] += 0.1;
+			gl.uniform3fv( thetaLoc,new Float32Array(theta));
+			s=[1.0,1.0,1.0];
+			gl.uniform3fv( sLoc,new Float32Array(s));
+			d=[0.0,0.0,0.0];
+			gl.uniform3fv( dLoc,new Float32Array(d));
+			gl.drawArrays(gl.TRIANGLES, 0, cubePoints.length / 3);
+			
 		}
 	
 	//}
